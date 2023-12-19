14ed222c8927de4a988905216e15508df0da825a
modelsplaid <modelsplaid@qq.com>
2023-12-19 16:50:06 +0800

    relocate onnx

--- onnx/onnx_export_super_reso/cvrt2.py
+++ onnx/onnx_export_super_reso/cvrt2.py
@@ -54,11 +54,11 @@ torch_model.eval()
 # Input to the model
 x = torch.randn(batch_size, 1, 224, 224, requires_grad=True)
 torch_out = torch_model(x)
-
+data_path = "../../data/super_resolution.onnx"
 # Export the model
 torch.onnx.export(torch_model,               # model being run
                   x,                         # model input (or a tuple for multiple inputs)
-                  "super_resolution.onnx",   # where to save the model (can be a file or file-like object)
+                  data_path,                 # where to save the model (can be a file or file-like object)
                   export_params=True,        # store the trained parameter weights inside the model file
                   opset_version=10,          # the ONNX version to export the model to
                   do_constant_folding=True,  # whether to execute constant folding for optimization
