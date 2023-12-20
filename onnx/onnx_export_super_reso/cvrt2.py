3956b061edc31b8b8b723e17ba95cdff75315827
modelsplaid <modelsplaid@qq.com>
2023-12-20 17:56:25 +0800

    todo: study runtimeonnx.py ln 29

--- onnx/onnx_export_super_reso/cvrt2.py
+++ onnx/onnx_export_super_reso/cvrt2.py
@@ -67,8 +67,9 @@ torch.onnx.export(
     output_names       = ['output'], # the model's output names
     dynamic_axes       = {'input' : {0 : 'batch_size'},    # variable length axes
                           'output': {0 : 'batch_size'}})
-
-### inference
+###################
+### inference test 
+###################
 
 batch_size = 1
 ort_session = onnxruntime.InferenceSession("../../data/super_resolution.onnx", providers=["CPUExecutionProvider"])
@@ -84,3 +85,40 @@ print("Done compute ONNX Runtime output prediction!")
 # compare ONNX Runtime and PyTorch results
 np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
 print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+
+
+###################
+### inference image
+###################
+
+from PIL import Image
+import torchvision.transforms as transforms
+
+img = Image.open("../../data/super_reso.png")
+
+resize = transforms.Resize([224, 224])
+img = resize(img)
+
+img_ycbcr = img.convert('YCbCr')
+img_y, img_cb, img_cr = img_ycbcr.split()
+
+to_tensor = transforms.ToTensor()
+img_y = to_tensor(img_y)
+img_y.unsqueeze_(0)
+
+ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img_y)}
+ort_outs = ort_session.run(None, ort_inputs)
+img_out_y = ort_outs[0]
+
+img_out_y = Image.fromarray(np.uint8((img_out_y[0] * 255.0).clip(0, 255)[0]), mode='L')
+
+# get the output image follow post-processing step from PyTorch implementation
+final_img = Image.merge(
+    "YCbCr", [
+        img_out_y,
+        img_cb.resize(img_out_y.size, Image.BICUBIC),
+        img_cr.resize(img_out_y.size, Image.BICUBIC),
+    ]).convert("RGB")
+
+# Save the image, we will compare this with the output image from mobile device
+final_img.save("../../data/out_super_reso.jpg")
--- onnx/onnx_export_super_reso/runtimeonnx.py
+++ onnx/onnx_export_super_reso/runtimeonnx.py
@@ -1,57 +1,47 @@
-# ref: https://pytorch.org/tutorials/advanced/super_resolution_with_onnxruntime.html
-import onnx
-import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
 import onnxruntime
-import torch
-import torch.nn as nn
-import torch.nn.init as init
-
-# onnx_model = onnx.load("../../data/super_resolution.onnx")
-# onnx.checker.check_model(onnx_model)
-batch_size = 1
-ort_session = onnxruntime.InferenceSession("../../data/super_resolution.onnx", providers=["CPUExecutionProvider"])
+import numpy as np 
 
 def to_numpy(tensor):
     return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+ort_session = onnxruntime.InferenceSession("../../data/super_resolution.onnx", providers=["CPUExecutionProvider"])
 
-class SuperResolutionNet(nn.Module):
-    def __init__(self, upscale_factor, inplace=False):
-        super(SuperResolutionNet, self).__init__()
 
-        self.relu = nn.ReLU(inplace=inplace)
-        self.conv1 = nn.Conv2d(1, 64, (5, 5), (1, 1), (2, 2))
-        self.conv2 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
-        self.conv3 = nn.Conv2d(64, 32, (3, 3), (1, 1), (1, 1))
-        self.conv4 = nn.Conv2d(32, upscale_factor ** 2, (3, 3), (1, 1), (1, 1))
-        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+img = Image.open("../../data/super_reso.png")
 
-        self._initialize_weights()
+resize = transforms.Resize([224, 224])
+img = resize(img)
 
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = self.pixel_shuffle(self.conv4(x))
-        return x
+img_ycbcr = img.convert('YCbCr')
+img_y, img_cb, img_cr = img_ycbcr.split()
 
-    def _initialize_weights(self):
-        init.orthogonal_(self.conv1.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv2.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv3.weight, init.calculate_gain('relu'))
-        init.orthogonal_(self.conv4.weight)
+to_tensor = transforms.ToTensor()
+img_y = to_tensor(img_y)
+img_y.unsqueeze_(0)
 
-torch_model = SuperResolutionNet(upscale_factor=3)
-x = torch.randn(batch_size, 1, 224, 224, requires_grad=True)
-torch_out = torch_model(x)
 
-# compute ONNX Runtime output prediction
-ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(img_y)}
 ort_outs = ort_session.run(None, ort_inputs)
+img_out_y = ort_outs[0]
 
-print("Done compute ONNX Runtime output prediction!")
+
+# todo: investigate here
+# todo: investigate here
+# todo: investigate here
+img_out_y = Image.fromarray(np.uint8((img_out_y[0] * 255.0).clip(0, 255)[0]), mode='L')
 
 
-# compare ONNX Runtime and PyTorch results
-#np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=1e-05)
-np.testing.assert_allclose(to_numpy(torch_out), ort_outs[0], rtol=1e-03, atol=9)
-print("Exported model has been tested with ONNXRuntime, and the result looks good!")
+print(img_out_y.size)
+print(Image.BICUBIC)
+
+# get the output image follow post-processing step from PyTorch implementation
+final_img = Image.merge(
+    "YCbCr", [
+        img_out_y,
+        img_cb.resize(img_out_y.size, Image.BICUBIC),
+        img_cr.resize(img_out_y.size, Image.BICUBIC),
+    ]).convert("RGB")
+
+# Save the image, we will compare this with the output image from mobile device
+final_img.save("../../data/out_super_reso.jpg")
